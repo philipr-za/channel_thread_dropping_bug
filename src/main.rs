1a1e3f76f4901919acd050385a3b6c2262dd04aa
Philip Robinson <probinson@blockstream.com>
2023-01-30 10:55:33 +0000

    more minimal example

--- src/main.rs
+++ src/main.rs
@@ -20,20 +20,18 @@ impl Drop for Foo {
 fn main() {
     let start = Instant::now();
     let (tx1, rx1) = mpsc::sync_channel(1);
-    let (tx2, rx2) = mpsc::sync_channel(0);
 
     let _ = thread::spawn(move || {
-        let _ = rx1;
+        // Move the receiver into this thread.
+        let _rx = rx1;
         thread::sleep(Duration::from_secs(1));
         panic!("expected panic");
-        // Under 1.66.1 sender `tx1` and the item in it's buffer are dropped after this panic
+        // Under 1.66.1 sender `tx1` and the item in it's buffer are dropped after this panic causes
+        // the thread to unwind and the Receiver to be dropped
         // Under 1.67.0 the sender `tx1` and the data in it's buffer are not dropped here
     });
-    let _ = thread::spawn(move || rx2.recv());
 
     tx1.send(Foo { data: 1, start }).unwrap();
-    tx2.send(Foo { data: 2, start }).unwrap();
-
     println!("Sends completed - {:?}", start.elapsed());
 
     thread::sleep(Duration::from_secs(5));
